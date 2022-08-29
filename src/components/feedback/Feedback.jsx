139bf268cbeb336b2950420adb11af34157f4679
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-08-29 22:59:29 +0200

    Refactoring to React hooks

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Feedback.module.css';
-import PropTypes from 'prop-types';
 
 import { Statistic } from 'components/statistics/Statistics';
 import { FeedbackOptions } from 'components/feedbackOptions/FeedbackOptions';
@@ -8,104 +7,66 @@ import { Section } from 'components/section/Section';
 import { Notification } from 'components/notification/Notification';
 import { Reset } from 'components/reset/Reset';
 
-export class Feedback extends Component {
-  static defaultProps = {
-    step: 1,
-    difficultGood: 0,
-    difficultNeutral: 0,
-    difficultBad: 0,
-  };
+export const Feedback = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  state = {
-    good: this.props.difficultGood,
-    neutral: this.props.difficultNeutral,
-    bad: this.props.difficultBad,
-  };
+  useEffect(() => {}, [good, neutral, bad]);
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
+  const countTotalFeedback = () => 
+    (good + neutral + bad);  
 
-  countPositiveFeedbackPercentage = () =>
-    (this.state.good / this.countTotalFeedback()) * 100;
+  const countPositiveFeedbackPercentage = () =>
+    (good / countTotalFeedback()) * 100;
 
-  updateFields = val => {
-    console.log(`${val} updating...`);
-    this.setState({
-      [val]: this.state[val] + this.props.step,
-    });
-  };
-
-  // updateNeutral = () => {
-  //   console.log('Neutral updating...');
-  //   this.setState((state, props) => ({
-  //     neutral: state.neutral + props.step,
-  //   }));
-  // };
-
-  // updateBad = () => {
-  //   console.log('Bad updating...');
-  //   this.setState((state, props) => ({
-  //     bad: state.bad + props.step,
-  //   }));
-  // };
-
-  resetState = () => {
+  const resetState = () => {
     console.log('Reset...');
-    this.setState(() => ({ bad: 0, good: 0, neutral: 0 }));
+    setGood(good - good);
+    setNeutral(neutral - neutral);
+    setBad(bad - bad);
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const { feedback, btnWrapper } = styles;
+  const { feedback, btnWrapper } = styles;
 
-    return (
-      <div className={feedback}>
-        <Section title="Please leave feedback">
-          <div className={btnWrapper}>
-            <FeedbackOptions
-              options="Good"
-              onLeaveFeedback={this.updateFields}
-              field="good"
-            />
-            <FeedbackOptions
-              options="Neutral"
-              onLeaveFeedback={this.updateFields}
-              field="neutral"
-            />
-            <FeedbackOptions
-              options="Bad"
-              onLeaveFeedback={this.updateFields}
-              field="bad"
-            />
-          </div>
-        </Section>
+  return (
+    <div className={feedback}>
+      <Section title="Please leave feedback">
+        <div className={btnWrapper}>
+          <FeedbackOptions
+            options="Good"
+            onLeaveFeedback={() => setGood(good + 1)}
+            field="good"
+          />
+          <FeedbackOptions
+            options="Neutral"
+            onLeaveFeedback={() => setNeutral(neutral + 1)}
+            field="neutral"
+          />
+          <FeedbackOptions
+            options="Bad"
+            onLeaveFeedback={() => setBad(bad + 1)}
+            field="nad"
+          />
+        </div>
+      </Section>
 
-        <Section title="Statistic">
-          {good || neutral || bad > 0 ? (
-            <>
-              <Statistic
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={this.countTotalFeedback()}
-                positivePercentage={this.countPositiveFeedbackPercentage().toFixed()}
-              />
-              <Reset name="Reset feedback" func={this.resetState} />
-            </>
-          ) : (
-            <Notification message="There is no feedback"></Notification>
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
-
-Feedback.propTypes = {
-  step: PropTypes.number.isRequired,
-  difficultGood: PropTypes.number.isRequired,
-  difficultNeutral: PropTypes.number.isRequired,
-  difficultBad: PropTypes.number.isRequired,
+      <Section title="Statistic">
+        {good || neutral || bad > 0 ? (
+          <>
+            <Statistic
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={countTotalFeedback()}
+              positivePercentage={countPositiveFeedbackPercentage().toFixed()}
+            />
+            <Reset name="Reset feedback" func={resetState} />
+          </>
+        ) : (
+          <Notification message="There is no feedback"></Notification>
+        )}
+      </Section>
+    </div>
+  );
 };
